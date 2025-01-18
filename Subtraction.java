fc43fe5ec9e26e8c2c3c443047edc987922b3457
AVIRALSINGH67 <aviral.raghuvansh1411@gmail.com>
2025-01-18 12:21:34 +0530

    Substraction

---
+++ Subtraction.java
@@ -0,0 +1,20 @@
+package calculator;
+import java.util.Scanner;
+
+public class Subtraction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+        
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+        
+        int difference = num1 - num2;
+        
+        System.out.println("The difference is: " + difference);
+        
+        scanner.close();
+    }
+}
